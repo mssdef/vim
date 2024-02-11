b6462aec0e98556c6b929866ee03ac156e3d9ca9
Andrew <dev@xpscommerce.com>
2024-02-11 14:17:25 +0100

    Create kill-processes.sh

---
+++ bin/kill-processes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+processes=("viber" "news" )
+
+for process_name in "${processes[@]}"; do
+    # Find the process IDs of the current process
+    process_pids=$(pgrep -i "$process_name")
+
+    # Check if any processes are running for the current name
+    if [ -n "$process_pids" ]; then
+        # Kill the processes
+        kill -9 $process_pids
+        echo "$process_name processes killed."
+    else
+        echo "No $process_name processes found."
+    fi
+done
